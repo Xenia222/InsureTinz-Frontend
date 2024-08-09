cec293bd3902118ca0116089d9b53b6ce191e447
Ittiqdev <ittiqdev28@outlook.com>
2024-08-09 17:18:04 +0100

    dkjbf

--- src/app/_helper/token.interceptor.ts
+++ src/app/_helper/token.interceptor.ts
@@ -1,26 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { TokenService } from '../_services/token.service';
 import { inject } from '@angular/core';
-import { StorageService } from '../_services/storage.service';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
 
 
   const token = inject(TokenService).getToken()
-  let langue = inject(StorageService).getLangue()
 
     if(token !== null){
-      let authReq = req.clone({
+      const authReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
-
-      if (langue){
-      const params = req.params.set('langue', langue);
-      authReq = authReq.clone({ params });
       console.log(authReq)
-    }
 
     return next(authReq);
 
